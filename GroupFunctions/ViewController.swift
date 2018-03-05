010bd90ddd13b2b3f477d4d1da440450e99382bc
aamayorga <amayorga95@outlook.com>
2018-03-05 14:40:20 -0800

    feat: Added group sorting
    Can now sort a dictionary of people by the first character of their last name.

--- GroupFunctions/ViewController.swift
+++ GroupFunctions/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Person {
+        let firstName: String
+        let lastName: String
+        let age: Int
+    }
+    
+    let people = [
+        Person(firstName: "Mark", lastName: "John", age: 34),
+        Person(firstName: "Bob", lastName: "Hamill", age: 24),
+        Person(firstName: "Jennifer", lastName: "Lane", age: 43),
+        Person(firstName: "Matthew", lastName: "Smith", age: 25),
+        Person(firstName: "James", lastName: "John", age: 23),
+        Person(firstName: "Keith", lastName: "Jackson", age: 29)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let groupedDictionary = Dictionary(grouping: people) { (person) -> Character in
+            return person.lastName.first!
+        }
+        
+        var groupedPeople = [[Person]]()
+        
+        let keys = groupedDictionary.keys.sorted()
+        keys.forEach { (key) in
+            groupedPeople.append(groupedDictionary[key]!)
+        }
+        
+        groupedPeople.forEach({
+            $0.forEach({print($0)})
+            print("---------------")
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
